2a5416e9192a303b2acb7278174ad0ec634ad90e
nagata4139 <nagata4139@gmail.com>
2020-11-29 23:00:25 +0900

    更新

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+from flask import Flask
 import requests
 
-# 郵便番号を取得
-zcode = str(input("郵便番号を入力してください。"))
+app = Flask(__name__)
 
-# 郵便番号をもとに情報を取得し出力
-zcode
-url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=" + zcode
-res = requests.get(url)
-resinfo = res.json()
-print(resinfo)
+@app.route(/)
+def top():
+    return "郵便番号(半角数字7桁)をもとに住所を検索します。" \
+           "ハイフンなしで入力してください。"
+    # 郵便番号を取得
+    zcode = str(input())
+
+@approute("/address_search?zipcode=<zcode>")
+def getzcode():
+    # 郵便番号をもとに情報を取得
+    url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=" + zcode
+    res = requests.get(url)
+    resinfo = res.json()
+
+    # 住所を抽出し値を戻す
+    for info in resinfo["results"]:
+        address = info["address1"] + info["address2"] + info["address3"]
+
+    return address
